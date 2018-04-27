980e87beb588a41f686496ca18a498ae87e3ede2
Felipe Cesar Fuerback <1749@grupo.schulze>
2018-04-27 17:15:53 -0300

    angular em tabelas

--- front-end/controller/dashboard.js
+++ front-end/controller/dashboard.js
@@ -1,6 +1,19 @@
 var dashboardModulo = angular.module('dashboardModulo', []);
 
 dashboardModulo.controller('dashboardController', function ($scope){
+  $scope.gastosUsuario = [
+    {descricao:'Visita', status:'waiting', valor:'312.00', categoria:'Outros', usuario:'Felipe'},
+    {descricao:'Almoço', status:'approved', valor:'215.00', categoria:'Alimentação', usuario:'Willian'},
+    {descricao:'Hotel', status:'canceled', valor:'312.00', categoria:'Hospedagem', usuario:'Kauan'},
+    {descricao:'Uber', status:'canceled', valor:'40.00', categoria:'Transporte', usuario:'Bruno'}
+  ];
+  $scope.gastosTotalUsuario = [
+    {valor:'312.00', email:'fuerback@gmail.com', usuario:'Felipe'},
+    {valor:'215.00', email:'willian@gmail.com', usuario:'Willian'},
+    {valor:'315.00', email:'kauan@gmail.com', usuario:'Kauan'},
+    {valor:'415.00', email:'bruno@gmail.com', usuario:'Bruno'},
+  ];
+
   $scope.showModal = function() {
     $('.modal').modal();
   }
